a56f13c591af6a76f7ef542fcfedc95f2cd78501
sebprt <sebastien.parrat@orange.fr>
2021-10-07 16:52:53 +0200

    Initial commit

---
+++ src/StateOutput/Workflow.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Component\State\StateOutput;
+
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+final class Workflow
+{
+    /** @var list<Pipeline> */
+    private array $pipelines = [];
+    private string $index = 'A';
+
+    public function __construct(
+        private ConsoleOutput $output,
+    ) {
+    }
+
+    public function withPipeline(string $label): Pipeline
+    {
+        return $this->pipelines[] = new Pipeline($this->output, $this->index++, $label);
+    }
+
+    public function update(): void
+    {
+        foreach ($this->pipelines as $pipeline) {
+            $pipeline->update();
+        }
+    }
+}
